f1aafc450f65241d6ccb3e82fef3e8a948e6a5a3
William Green <will.g.green@gmail.com>
2025-08-02 12:56:24 -0400

    declaring and using components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppModule {}
